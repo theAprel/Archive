3f8475d323c383c932ce0f88e6299bec6f461d3e
theAprel <cpindziak@alumni.ufl.edu>
2017-07-21 00:37:02 -0400

    Make completion smarter.

--- src/aprel/ConsoleInterface.java
+++ src/aprel/ConsoleInterface.java
@@ -19,20 +19,23 @@ package aprel;
 import aprel.db.beans.DbFile;
 import aprel.db.beans.DirectoryBean;
 import aprel.db.beans.DirectoryStructure;
+import aprel.db.beans.FileBean;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.jline.reader.Candidate;
+import org.jline.reader.Completer;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.ParsedLine;
 import org.jline.reader.UserInterruptException;
-import org.jline.reader.impl.completer.ArgumentCompleter;
-import org.jline.reader.impl.completer.StringsCompleter;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
@@ -43,28 +46,35 @@ import org.jline.terminal.TerminalBuilder;
 public class ConsoleInterface {
     private final LinkedList<DirectoryStructure> dirPath;
     private final DirectoryBean root;
-    private final Map<String,DirectoryBean> directoryNames;
     private final ArchiveDatabase db;
     private final boolean useConsole;
     private final LineReader reader;
     private final DbCompleter filesCompleter = new DbCompleter();
     private static final String ILLEGAL_NUMBER_OF_ARGUMENTS = "Illegal number of arguments";
     
+    public static final String[] COMMANDS = new String[] {
+        "cd", "exit", "rmdir", "ls", "mkdir", "mv", "rename"
+    };
+    public static final String[] DIRECTORY_COMMANDS = new String[] {
+        "cd", "rmdir", "mv", "rename"
+    };
+    public static final String[] FILE_COMMANDS = new String[] {
+        "mv", "rename"
+    };
+    
     public ConsoleInterface(DirectoryBean root, ArchiveDatabase database, boolean useConsole) throws IOException {
         this.root = root;
         db = database;
         dirPath = new LinkedList<>();
-        directoryNames = new HashMap<>();
         dirPath.add(new DirectoryStructure(root, db));
         this.useConsole = useConsole;
         if(useConsole) {
             updateCompleters();
             Terminal term = TerminalBuilder.builder().system(true).build();
-            StringsCompleter commands = new StringsCompleter("cd", "exit", "rmdir", "ls", "mkdir");
             reader = LineReaderBuilder.builder()
                     .terminal(term)
                     .appName("Archive")
-                    .completer(new ArgumentCompleter(commands, filesCompleter))
+                    .completer(filesCompleter)
                     .build();
         }
         else
@@ -142,14 +152,14 @@ public class ConsoleInterface {
             String name = name1.trim();
             if(name.length() == 0) continue;
             DirectoryBean result = thisDir.createDirectory(name);
-            directoryNames.put(name, result);
+            filesCompleter.add(result);
         }
         updateCompleters();
     }
     
     public void rmdir(String[] names) {
         for(String name : names) {
-            DirectoryBean dir = directoryNames.get(name);
+            DirectoryBean dir = filesCompleter.getDirectoryByName(name);
             if(dir == null) {
                 System.out.println("No directory by name " + name);
                 continue;
@@ -200,7 +210,7 @@ public class ConsoleInterface {
             dirPath.removeLast();
         }
         else {
-            DirectoryBean changeTo = directoryNames.get(enter);
+            DirectoryBean changeTo = filesCompleter.getDirectoryByName(enter);
             if(changeTo == null)
                 return "Directory \"" + enter + "\" does not exist.\n";
             DirectoryStructure ds = new DirectoryStructure(changeTo, db);
@@ -212,29 +222,62 @@ public class ConsoleInterface {
     
     private void updateCompleters() {
         DirectoryStructure ds = dirPath.getLast();
-        directoryNames.clear();
-        filesCompleter.clearCandidates();
-        ds.getFiles().stream().forEach(f -> {
-            final String name = f.getName();
-            filesCompleter.addCandidate(name);
-            if(f instanceof DirectoryBean)
-                directoryNames.put(name, (DirectoryBean) f);
-        });
+        filesCompleter.update(ds.getFiles());
     }
     
-    private static class DbCompleter extends StringsCompleter {
-
+    private static class DbCompleter implements Completer {
+        
+        private final Map<String,DirectoryBean> directoryNames;
+        private final Map<String,FileBean> filenames;
+        
         public DbCompleter() {
-            super();
+            directoryNames = new HashMap<>();
+            filenames = new HashMap<>();
         }
         
-        public void addCandidate(String c) {
-            candidates.add(new Candidate(c));
+        public void add(DbFile f) {
+            if(f instanceof FileBean) filenames.put(f.getName(), (FileBean) f);
+            else if(f instanceof DirectoryBean) directoryNames.put(f.getName(), (DirectoryBean) f);
+            else throw new IllegalArgumentException("Unrecognized class: " + f.getClass());
         }
         
-        public void clearCandidates() {
-            candidates.clear();
+        public void clear() {
+            directoryNames.clear();
+            filenames.clear();
         }
         
+        public void update(Collection<DbFile> files) {
+            clear();
+            files.forEach(f -> add(f));
+        }
+        
+        public DirectoryBean getDirectoryByName(String name) {
+            return directoryNames.get(name);
+        }
+
+        @Override
+        public void complete(LineReader reader, ParsedLine line, List<Candidate> candidates) {
+            List<String> words = line.words();
+            if(words == null) return;
+            final int wordIndex = line.wordIndex();
+            if(wordIndex == 0) {
+                candidates.addAll(Arrays.stream(COMMANDS).map(
+                        com -> new Candidate(com)).collect(Collectors.toList()));
+                return;
+            }
+            else {
+                final String command = words.get(0);
+                Set<String> cands = new HashSet<>();
+                if(Arrays.asList(DIRECTORY_COMMANDS).contains(command)) {
+                    cands.addAll(directoryNames.keySet());
+                }
+                if(Arrays.asList(FILE_COMMANDS).contains(command)) {
+                    cands.addAll(filenames.keySet());
+                }
+                candidates.addAll(cands.stream().map(c -> new Candidate(c))
+                        .collect(Collectors.toSet()));
+            }
+        }
+
     }
 }
